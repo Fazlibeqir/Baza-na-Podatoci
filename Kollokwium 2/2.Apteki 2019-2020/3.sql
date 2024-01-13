edf2850ae4848d86a60ed9b1d96dcf92e3a44bba
fazlibeqir <fazlibeqir@gmail.com>
2024-01-13 19:57:35 +0100

    Update Apteka3.sql

--- Kollokwium 2/2.Apteki 2019-2020/3.sql
+++ Kollokwium 2/2.Apteki 2019-2020/3.sql
@@ -5,14 +5,16 @@ join (
  select
  p.id_pacient , 
  p.ime_pacient ,
- p.id_grad ,
+ a.id_grad ,
  count(l.id_lek)as lekovite 
 	from pacienti p
 	inner join recepti r on p.id_pacient =r.id_pacient 
 	left join prodazba p2 on r.id_recept =p2.id_recept 
+	left join smetki s on s.id_smetka=p2.id_smetka
+	left join apteki a on a.id_apteka = s.id_apteka
 	left join lekovi l on p2.id_lek =l.id_lek 
 	group by 1,2,3
 	)as brLek on g.id_grad =brLek.id_grad
 where 
 (g.ime_grad ilike 'skopje' or g.ime_grad  ilike 'stip')
-and brLek.lekovite>=10;
+and brLek.lekovite>10;
